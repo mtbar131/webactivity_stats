7c36663fad8f4b1f3a9b1872437f279f2be298fd
Amit Barve <mtbar131@gmail.com>
2017-02-15 10:05:40 -0500

    website visit stats chrome extension - first commit

---
+++ popup.js
@@ -0,0 +1,99 @@
+var recordsDictionary = {};
+var lastURL = null;
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	getTabInfo();
+    });
+
+    chrome.tabs.onActiveChanged.addListener(function(tabId, selectInfo) {
+	getTabInfo();
+    });
+});
+
+function processEvent(url) {
+    if (lastURL != null) {
+	var stats = {};
+	/* update used time for last active tab */
+	stats = recordsDictionary[lastURL];
+	stats['timespent'] = new Date().valueOf() - stats['timestamp'];
+	recordsDictionary[lastURL] = stats;
+    }
+
+    if (!(url in recordsDictionary)) {
+	var stats = {};
+	stats['count'] = 1;
+	stats['timespent'] = 0;
+	stats['timestamp'] = new Date().valueOf();
+	recordsDictionary[url] = stats;
+    } else {
+	var stats = recordsDictionary[url];
+	stats['count'] = stats['count'] + 1;
+	stats['timestamp'] = new Date().valueOf();
+	recordsDictionary[url] = stats;
+    }
+    
+    console.log('Here are all values till now\n');
+    for(key in recordsDictionary){
+	var stats = recordsDictionary[key];
+	console.log('URL: ' + key + ' Data: ' + JSON.stringify(stats));
+    }
+    console.log('----------------------------\n');
+    lastURL = url;
+}
+
+function getTabInfo() {
+
+    var queryInfo = {
+	active: true,
+	currentWindow: true
+    };
+
+    chrome.tabs.query(queryInfo, function(tabs) {
+	// chrome.tabs.query invokes the callback with a list of tabs that match the
+	// query. When the popup is opened, there is certainly a window and at least
+	// one tab, so we can safely assume that |tabs| is a non-empty array.
+	// A window can only have one active tab at a time, so the array consists of
+	// exactly one tab.
+	var tab = tabs[0];
+
+	if (typeof tab == 'undefined') {
+	    console.log('tab is undefined');
+	    return;
+	}
+
+	if (tab.url == 'chrome://newtab/') {
+	    console.log('New tab without url');
+	    return;
+	}
+
+	// A tab is a plain object that provides information about the tab.
+	// See https://developer.chrome.com/extensions/tabs#type-Tab
+	var url = tab.url;
+
+	// tab.url is only available if the "activeTab" permission is declared.
+	// If you want to see the URL of other tabs (e.g. after removing active:true
+	// from |queryInfo|), then the "tabs" permission is required to see their
+	// "url" properties.
+	console.assert(typeof url == 'string', 'tab.url should be a string');
+	
+	processEvent(extractDomain(url));
+    });
+}
+
+function extractDomain(url) {
+    var domain;
+    //find & remove protocol (http, ftp, etc.) and get domain
+    if (url.indexOf("://") > -1) {
+        domain = url.split('/')[2];
+    }
+    else {
+        domain = url.split('/')[0];
+    }
+
+    //find & remove port number
+    domain = domain.split(':')[0];
+
+    return domain;
+}
